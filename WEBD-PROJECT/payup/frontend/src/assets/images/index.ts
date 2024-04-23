22a8676d29334373a40161c6dc4e08ca80a23b1b
BHARATH GADDAM <134089638+bharathgaddam1712@users.noreply.github.com>
2024-04-23 11:22:04 +0530

    Delete WEBD-PROJECT/payup directory

--- WEBD-PROJECT/payup/frontend/src/assets/images/index.ts
+++
@@ -1,5 +0,0 @@
-import transferIllustration from "./illustration.jpg";
-import spinnerLg from "./spinner-lg.svg";
-import spinnerSm from "./spinner-sm.svg";
-
-export { transferIllustration, spinnerLg, spinnerSm };
